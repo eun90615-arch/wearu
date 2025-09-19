7a8c866c51fcabe9e6d000742cbb841f050c9334
성은 <eun90615@gmail.com>
2025-09-19 11:34:38 +0900

    이미지 경로 수정

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -8,7 +8,7 @@ const Meals = () => {
   const[showAll, setShowAll] = useState(false); //더보기 버튼 클릭 상태에 따라 true/false
 
   useEffect(()=>{
-    fetch('/db.json')
+    fetch(`${process.env.PUBLIC_URL}/db.json`)
     .then(r=>r.json())//json 형식으로 데이터 변환
     .then(data => setAll(data.meals || [])) //json에서 meals 배열만 꺼내서 상태에 저장
     .catch(console.error);
